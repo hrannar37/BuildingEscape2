ecba1406f39c7b010fed89bf22b06e29f7794226
Alexander <eplabaka11@gmail.com>
2017-10-22 00:47:05 +0000

    minor map changes

---
+++ backup/PositionReport.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+
+#include "Engine/World.h" //TODO REMOVE THIS, only for autocompleto
+#include "GameFramework/Actor.h"
+#include "Components/ActorComponent.h"
+#include "PositionReport.generated.h"
+
+
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDING_ESCAPE_API UPositionReport : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UPositionReport();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+	
+};
